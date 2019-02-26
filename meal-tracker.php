e925083eb64b63d521a8603508093bdfcb89ef16
Ali Colville <acolville@dctmedia.co.uk>
2019-02-26 14:59:10 +0000

    Initial files

---
+++ meal-tracker.php
@@ -0,0 +1,46 @@
+<?php
+
+defined('ABSPATH') or die("Jog on!");
+
+/**
+ * Plugin Name: Meal Tracker
+ * Description: // TODO
+ * Version: 0.1
+ * Author: YeKen
+ * Author URI: http://www.YeKen.uk
+ * License: GPL2
+ * Text Domain: meal-tracker
+ */
+/*  Copyright 2019 YeKen.uk
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+define( 'YK_MT_ABSPATH', plugin_dir_path( __FILE__ ) );
+
+define( 'YK_MT_PLUGIN_VERSION', '0.1' );
+define( 'YK_MT_SLUG', 'meal-tracker' );
+
+// -----------------------------------------------------------------------------------------
+// AC: Include all relevant PHP files
+// -----------------------------------------------------------------------------------------
+
+#include_once SH_CD_ABSPATH . 'includes/class.presets.php';
+
+
+// -----------------------------------------------------------------------------------------
+// AC: Load relevant language files
+// -----------------------------------------------------------------------------------------
+
+load_plugin_textdomain( YK_MT_SLUG, false, dirname( plugin_basename( __FILE__ ) ) . '/includes/languages/' );
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Meal Tracker ===
+Contributors: aliakro
+Donate link: https://www.paypal.me/yeken
+Tags: meal, tracker, calories, weight, food
+Requires at least: 4.2.0
+Tested up to: 5.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Create your own shortcodes and assign text, html, etc to them. Use these across your site and only change in one place.
+
+== Upgrade Notice ==
+
+
+== Description ==
+
+
+== Installation ==
+
+1. Login into Wordpress Admin Panel
+2. Navigate to Plugins > Add New
+3. Search for "Meal Tracker"
+4. Click Install now and activate plugin
+
+== Frequently Asked Questions ==
+
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 0.1 =
+
+* Initial Build
