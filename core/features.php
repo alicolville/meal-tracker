1c2e1fa6843dd8d64c7a159baf52e6c6f9404cb9
Al <email@yeken.uk>
2019-10-13 20:46:06 +0100

    Updated features

--- core/features.php
+++ core/features.php
@@ -4,6 +4,7 @@ Done:
 - Works with Weight Tracker. Source.
 - View all entries and edit entries for different days
 - Compress multiple line items into one
+- View and Edit entries for any day.
 
 Todo:
 
