300441e58a6e4ac626e652f2c1cbcc518c46dd81
Al <email@yeken.uk>
2019-10-23 19:51:54 +0100

    Typo fix

--- core/cron.php
+++ core/cron.php
@@ -3,10 +3,10 @@
     defined('ABSPATH') or die("Jog on!");
 
     // Fetch the existing license from WP Options and run it through validation again.
-    function yk_my_cron_licence_check() {
+    function yk_mt_cron_licence_check() {
 
         $existing_license = yk_mt_license();
 
         yk_mt_license_apply( $existing_license );
     }
-    add_action( 'daily', 'yk_my_cron_licence_check' );
+    add_action( 'daily', 'yk_mt_cron_licence_check' );
