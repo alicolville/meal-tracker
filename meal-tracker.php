a8ce2e71faf7ba9762fbefb87d8467fd8d007764
Al <email@yeken.uk>
2019-11-27 12:57:28 +0000

    Updated Versions

--- meal-tracker.php
+++ meal-tracker.php
@@ -5,7 +5,7 @@ defined('ABSPATH') or die("Jog on!");
 /**
  * Plugin Name: Meal Tracker
  * Description: // TODO
- * Version: 0.4-beta2
+ * Version: 0.4
  * Author: YeKen
  * Author URI: http://www.YeKen.uk
  * License: GPL2
@@ -28,7 +28,7 @@ defined('ABSPATH') or die("Jog on!");
 */
 
 define( 'YK_MT_ABSPATH', plugin_dir_path( __FILE__ ) );
-define( 'YK_MT_PLUGIN_VERSION', '0.4-beta2' );
+define( 'YK_MT_PLUGIN_VERSION', '0.4' );
 define( 'YK_MT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'YK_MT_SLUG', 'meal-tracker' );
 
