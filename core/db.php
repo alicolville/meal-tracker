676e9568ab1aff78cf9a2c0775216dc03f02f12a
Al <email@yeken.uk>
2020-01-03 15:45:06 +0000

    Added MacroN to DB schema

--- core/db.php
+++ core/db.php
@@ -923,7 +923,10 @@ function yk_mt_db_mysql_formats( $data ) {
         'deleted'               => '%d',
         'favourite'             => '%d',
         'unit'                  => '%s',
-        'json'                  => '%s'
+        'json'                  => '%s',
+        'protein'               => '%f',
+        'fat'                   => '%f',
+        'carbs'                 => '%f'
     ];
 
     $return = [];
@@ -959,6 +962,9 @@ function yk_wt_db_tables_create() {
                 added_by int NOT NULL,
                 name varchar(60) NOT NULL, 
                 calories float DEFAULT 0 NOT NULL,
+                protein float DEFAULT 0 NOT NULL,
+                fat float DEFAULT 0 NOT NULL,
+                carbs float DEFAULT 0 NOT NULL,
                 quantity float DEFAULT 0 NOT NULL,
                 unit varchar(10) DEFAULT 'g' NOT NULL, 
                 description varchar(200) NULL,
