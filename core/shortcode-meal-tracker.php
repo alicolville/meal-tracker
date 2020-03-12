09f615005f77cb5dcf7f0d8b0b332a6fffadb27f
Robert Paul <rpauldesign@gmail.com>
2020-03-12 20:42:35 +0000

    More summary bits

--- core/shortcode-meal-tracker.php
+++ core/shortcode-meal-tracker.php
@@ -127,7 +127,7 @@
 
         return '
             <div class="yk-mt__summary">
-                <div class="yk-mt__summary-title"><span class="fa fa-check-circle"></span> Your Summary</div>
+                <div class="yk-mt__summary-title"><span class="fa fa-check-circle"></span> Today\'s summary</div>
                 <div class="yk-mt__table yk-mt__table--summary">
                     <div class="yk-mt__table-row" >
                         <div class="yk-mt__table-cell yk-mt__table--summary-chart-slot">
