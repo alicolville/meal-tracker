a2a3cdb752eb705e7961e0ba093fcc0307bad272
Ali Colville <acolville@dctmedia.co.uk>
2020-09-28 20:59:46 +0100

    Removed redundant code

--- core/admin-pages/meals/meals-home.php
+++ core/admin-pages/meals/meals-home.php
@@ -11,18 +11,9 @@ function yk_mt_admin_page_meals_home() {
 
   	// Call relevant page function
 	switch ( yk_mt_querystring_value( 'mode', 'dashboard' ) ) {
-//        case 'user':
-//            yk_mt_admin_page_user_summary();
-//            break;
         case 'meal':
             yk_mt_admin_page_meals_add_edit();
             break;
-//        case 'search-results':
-//            yk_mt_admin_page_search_results();
-//            break;
-//        case 'settings':
-//            // Call settings page
-//            break;
 		default:
             yk_mt_admin_page_meals_dashboard();
 			break;
