4c38ef7cd7649444683b2e019490deb2bb8c1d3e
Ali Colville <acolville@dctmedia.co.uk>
2021-10-18 15:48:05 +0100

    Version bump

--- meal-tracker.php
+++ meal-tracker.php
@@ -6,7 +6,7 @@ defined('ABSPATH') or die("Jog on!");
  * Plugin Name:         Meal Tracker
  * Description:         Allow your users to track their meals and calorie intake for a given day.
  * Version:             3.0.6
- * Requires at least:   5.2
+ * Requires at least:   5.7
  * Tested up to:		5.8.1
  * Requires PHP:        7.4
  * Author:              Ali Colville
@@ -18,7 +18,7 @@ defined('ABSPATH') or die("Jog on!");
  */
 
 define( 'YK_MT_ABSPATH', plugin_dir_path( __FILE__ ) );
-define( 'YK_MT_PLUGIN_VERSION', '3.0.6' );
+define( 'YK_MT_PLUGIN_VERSION', '3.0.7' );
 define( 'YK_MT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'YK_MT_SLUG', 'meal-tracker' );
 
