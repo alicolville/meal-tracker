5c0b0e187c2ab55a3ff368da7ebb9eeaec30a005
Ali Colville <acolville@dctmedia.co.uk>
2021-12-04 17:22:14 +0000

    * Updated version WP compatibility statement.

--- meal-tracker.php
+++ meal-tracker.php
@@ -5,10 +5,10 @@ defined('ABSPATH') or die("Jog on!");
 /**
  * Plugin Name:         Meal Tracker
  * Description:         Allow your users to track their meals and calorie intake for a given day.
- * Version:             3.0.6
+ * Version:             3.0.7
  * Requires at least:   5.7
- * Tested up to:		5.8.1
- * Requires PHP:        7.4
+ * Tested up to:		5.8.2
+ * Requires PHP:        7.2
  * Author:              Ali Colville
  * Author URI:          https://www.YeKen.uk
  * License:             GPL v2 or later
