6488c48749e2d4b4e09e0c9ab0653f222f24b28e
Ali Colville <email@yeken.uk>
2024-09-03 09:04:25 +0100

    version bump

--- meal-tracker.php
+++ meal-tracker.php
@@ -5,7 +5,7 @@ defined('ABSPATH') or die("Jog on!");
 /**
  * Plugin Name:         Meal Tracker
  * Description:         Allow your users to track their meals and calorie intake for a given day.
- * Version:             3.1.7
+ * Version:             3.2
  * Requires at least:   5.7
  * Tested up to:		6.5
  * Requires PHP:        7.2
@@ -18,7 +18,7 @@ defined('ABSPATH') or die("Jog on!");
  */
 
 define( 'YK_MT_ABSPATH', plugin_dir_path( __FILE__ ) );
-define( 'YK_MT_PLUGIN_VERSION', '3.1.7' );
+define( 'YK_MT_PLUGIN_VERSION', '3.2' );
 define( 'YK_MT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'YK_MT_SLUG', 'meal-tracker' );
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.paypal.me/yeken
 Tags: meal, tracker, calories, weight, food
 Requires at least: 5.7
 Tested up to: 6.5
-Stable tag: 3.1.7
+Stable tag: 3.2
 Requires PHP: 7.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -86,6 +86,12 @@ The plugin is written in English (UK) but is ready for translation. If you wish 
 3.1 - Communicate and search with meal collections from other WP installs with Meal Tracker installed.
 
 == Changelog ==
+
+= 3.2 =
+
+Several fixes based upon WordPress's plugin feedback:
+
+
 
 = 3.1.7 =
 
